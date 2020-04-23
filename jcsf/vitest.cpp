fb80ab9c8735aa28e0e79caf8cdfa4d4a32d7dbc
lyh666888 <383912796@qq.com>
2020-04-23 08:38:22 +0800

    。

--- jcsf/vitest.cpp
+++ jcsf/vitest.cpp
@@ -1,13 +1,7 @@
 #include<iostream>
-#include<stack>
-using namespace std;
-stack <int >s;
-int main()
-{
-    cout<<s.empty()<<endl;
-    if(s.empty())
-    {
-        cout<<s.empty()<<endl;
-    }
-    return 0;
-}
+    #include<stack>
+        using namespace std;
+        stack <int>s;
+            int main()
+            {
+            cout<<s.empty()<<endl; if(s.empty()) { cout<<s.empty()<<endl; } return 0; }
